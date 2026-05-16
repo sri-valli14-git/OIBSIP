adaa92e09aec39b2defa988a7bab794f905b6383
Srivalli <jajalasrivalli0705@gmail.com>
2026-05-16 21:11:41 +0530

    Added Task 1 BMI Calculator

---
+++ TASK1_BMI_CALCULATOR/README.md
@@ -0,0 +1,31 @@
+# BMI Calculator — Task 1
+A command-line BMI(Body Mass Index)Calculator has been built in Python as part of the Oasis Infobyte Python Programming Internship.
+
+# Author
+Jajala Srivalli - Oasis Infobyte Python Programming Internship.
+
+# How to run
+Enter: python srivalli_Task1.py
+
+# Requirements
+1.Python 3.x..version, My python version is 3.14.3
+2.No external libraries needed
+
+# Features
+1.Accepts weight in kilograms and height in meters
+2.Calculates BMI using the formula: (weight / height^2)
+3.Classifies result into 4 categories:
+  ->Underweight (BMI < 18.5)
+  ->Normal weight ( 18.5 <= BMI < 25 )
+  ->Overweight ( 25 <= BMI < 30 )
+  ->Obesity (BMI ≥ 30)
+4.Handles invalid input, using try/except
+5.Validates the values which are Positive and Realistic
+
+# Sample output
+Enter your weight in kgs: 49
+Enter your height in meters: 1.5
+BMI =  21.78
+Category =  Normal
+
+# End
---
+++ TASK1_BMI_CALCULATOR/srivalli_Task1.py
@@ -0,0 +1,51 @@
+#Oasis Infobyte Python Programming Internship - TASK(1)
+#PROJECT: BMI CALCULATOR
+#Author: Srivalli
+
+def calculate_bmi(weight,height):
+    bmi=((weight)/(height**2))
+    bmi=round(bmi,2)
+
+    if(bmi<18.5):
+        category="Underweight"
+    elif(bmi>=18.5 and bmi<25):
+        category="Normal"
+    elif(bmi>=25 and bmi<30):
+        category="Overweight"
+    else:
+        category="Obesity"
+
+    return bmi,category
+
+
+print("BMI CALCULATOR")
+
+try:
+        weight=float(input("Enter your weight in kgs: "))
+        height=float(input("Enter your height in meters: " ))
+
+        if(weight<=0 or height <=0 or height>3.0):
+            print("Invalid Input Values.")
+        else:
+             bmi,category=calculate_bmi(weight,height)
+             print("BMI = ",bmi)
+             print("Category = ",category)
+
+
+except ValueError:
+     print("Enter only Numbers..")
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
